f53ec71483fe99283e57285f1cd55ea75160cb11
Luong Hoang <43042403+kakakaka1234@users.noreply.github.com>
2019-09-19 13:44:13 -0600

    update

---
+++ scripts/IntroLessons/operators.js
@@ -0,0 +1,33 @@
+console.log("You made it to the console");
+
+
+
+function simpleOperators(numberA,numberB){
+
+  let adding = numberA + numberB;
+  let substraction=  numberA - numberB;
+  let multiplying =  numberA * numberB;
+  let division=  numberA / numberB;
+  let modulus=  numberA % numberB;
+  console.log(numberA,numberB,'adding', adding);
+  console.log(numberA,numberB,'substraction', substraction);
+  console.log(numberA,numberB,'multiplying', multiplying);
+  console.log(numberA,numberB,'division', division);
+  console.log(numberA,numberB, 'modulus',modulus);
+
+
+
+
+
+}
+skip count by ____ for ______ times starting at _____
+function skipCpinting()
+
+let number=[4,7,13,99,0,100,5,1,'1'];
+
+simpleOperators(number[0], number[1]);
+simpleOperators(number[1], number[2]);
+simpleOperators(number[0], number[2]);
+simpleOperators(number[0], number[3]);
+simpleOperators(number[0], number[4]);
+simpleOperators(number[0], number[5]);
