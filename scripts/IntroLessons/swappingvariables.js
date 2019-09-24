4df4546c013be5f229aa1d47d79d91e6eab75514
Luong Hoang <43042403+kakakaka1234@users.noreply.github.com>
2019-09-24 12:59:00 -0600

    update

--- scripts/IntroLessons/swappingvariables.js
+++ scripts/IntroLessons/swappingvariables.js
@@ -1,1 +1,54 @@
 console.log("You made it to the console");
+
+console.log('');
+console.log('');
+
+
+let person1 = {
+  firstName: "Luong",
+  lastName: "Hoang",
+  age: 17, //Not my real age :)
+  favoriteNumber: 22, //security feature
+  haircolor: null, // explore: undefined, null; origonally "none"
+  // debug with typeof
+  male: true,
+  female: false,
+  other: false,
+  country: undefined,
+  sport: undefined,
+};
+
+let person2 = {
+  firstName: "Viet",
+  lastName: "Truong",
+  age: 17, //Not my real age :)
+  favoriteNumber: 0, //security feature
+  haircolor: null, // explore: undefined, null; origonally "none"
+  // debug with typeof
+  male: true,
+  female: false,
+  other: false,
+  country:undefined,
+  sport:undefined,
+};
+console.log(person1.firstName);
+console.log(person1.lastName);
+console.log(person1.age);
+console.log(person1.favoriteNumber);
+console.log(person1.haircolor);
+console.log(person1.male);
+console.log(person1.female);
+console.log(person1.other);
+console.log(person1.country);
+console.log(person1.sport);
+
+console.log(person2.firstName);
+console.log(person2.lastName);
+console.log(person2.age);
+console.log(person2.favoriteNumber);
+console.log(person2.haircolor);
+console.log(person2.male);
+console.log(person2.female);
+console.log(person2.other);
+console.log(person2.country);
+console.log(person2.sport);
