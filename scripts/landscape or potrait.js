f844091b875b51d13feb953a4ce7ec69ea2f916c
Luong Hoang <43042403+kakakaka1234@users.noreply.github.com>
2019-09-30 13:16:08 -0600

    update

--- scripts/landscape or potrait.js
+++ scripts/landscape or potrait.js
@@ -1,30 +1,23 @@
-console.log("You made it to the console!");
+let stop = false; //NAN Boolean Memory
 
-
-
-
-let stop=false;
-function testNaN(number) {
-  console.log(stop);
-  if (isNaN(number)){
-    stop= true;
-
-    return"type a new number"
+function testNaN (number) {
+  //console.log(stop);
+  if ( isNaN (number) ) {
+    stop = true;
+    return "Type a real number"
+  } else {
+    return "Input Validated"
   }
-else {
-  return" Input Validated"
-
-
 }
 
-
-
-}
 function geometry (width, height) { //Notice these parameters are local and do not mix with "number" variable
   if (width == height) return 'Square'; //must be first, return will not allow rest of code to run
-  return (width > height) ? 'Landscape' : 'Portrait'; //ternary operator example
-  //Seems odd, the inequality, here, is reading the opposite
+  return (width > height) ? 'Landscape' :'Potrait';
+
 }
+   //ternary operator example
+  //Seems odd, the inequality, here, is reading the opposite
+
 
 function checkboxes() {
   let checkbox1, checkbox2, checkbox3;
@@ -48,29 +41,33 @@ function checkboxes() {
   }
 }
 
-
+// This coding illustrates the advanced method
 function main() {
-  console.log("it works");
-  let firstNumber;
-  let secondNumber;
-  let text;
+  //console.log("It works");
 
-  firstNumber= document.getElementByID("textfield").value;
-  document.getElementByID("validityTest").innerHTML= testNaN(firstNumber);
-    document.getElementByID("validityTest").innerHTML= alert(testNaN(firstNumber));
-  //testNaN(firstNumber);
-
-  secondNumber= document.getElementByID("textfield2").value;
-  document.getElementByID("validityTest2").innerHTML= testNaN(secondNumber);
-    document.getElementByID("validityTest2").innerHTML= alert(testNaN(secondNumber));
-    if(stop == true){
-      document.getElementByID("answer").innerHTML="restart the example, a width or height needs to be a number."
-    } else {
-      console.log("the display geometry is",geometry(firstNumber,secondNumber));
-      document.getElementByID("answer").innerHTML ="This display is" + geometry (firstNumber,secondNumber);
-
-    }
+  //checkboxes();
 
 
+  let firstNumber, secondNumber, text;
+
+  //Get the Width-value from id=textfield1
+  firstNumber = document.getElementById('textfield').value;
+
+  //testNaN (firstNumber);
+  document.getElementById('validityTest').innerHTML = alert ( testNaN (firstNumber));
+  document.getElementById('validityTest').innerHTML = testNaN (firstNumber);
+
+  //Get the Height-value from id=textfield2
+  secondNumber = document.getElementById('textfield2').value;
+  //testNaN (secondNumber);
+  document.getElementById('validityTest2').innerHTML = alert ( testNaN (secondNumber));
+  document.getElementById('validityTest2').innerHTML = testNaN (secondNumber);
+
+  if (stop == true) {
+    document.getElementById('answer').innerHTML = "Restart the example, a width or height needs to be a number."
+  } else {
+    console.log("The display geometry is", geometry (firstNumber, secondNumber) );
+    document.getElementById('answer').innerHTML = "This display is " + geometry (firstNumber, secondNumber);
+  }
 
 }
