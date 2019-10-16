d70091226c6603e2545ea7474c6544dc8577d9e1
Luong Hoang <43042403+kakakaka1234@users.noreply.github.com>
2019-10-16 13:50:41 -0600

    update

--- scripts/modulus.js
+++ scripts/modulus.js
@@ -1,1 +1,44 @@
 console.log("You made it to the console!");
+console.log("You made it to the console!");
+
+let stop = false; //NAN Boolean Memory
+
+function testNaN (number) {
+  //console.log(stop);
+  if ( isNaN (number) ) {
+    stop = true;
+    return "Type a real number"
+  } else {
+    return "Input Validated"
+  }
+}
+function geometry (firstNumber,secondNumber) {
+  let number= firstNumber % 2
+  let number1=secondNumber% 2
+  if (number == 1) return 'odd';
+  if (number == 0) return 'even';
+  if (number1 == 1) return 'odd';
+  if (number1 == 0) return 'even'}
+
+function main() {
+  let firstNumber, secondNumber;
+
+  firstNumber = document.getElementById('textfield1').value;
+
+  document.getElementById('validityTest1').innerHTML = alert ( testNaN (firstNumber));
+  document.getElementById('validityTest1').innerHTML = testNaN (firstNumber);
+
+  secondNumber = document.getElementById('textfield2').value;
+  document.getElementById('validityTest2').innerHTML = alert ( testNaN (secondNumber));
+  document.getElementById('validityTest2').innerHTML = testNaN (secondNumber);
+
+  if (stop == true) {
+    document.getElementById('answer').innerHTML = "Restart the example, a first and second numbers needs to be a number."
+  } else {
+    console.log("The firstnumber is", geometry (firstNumber, secondNumber) );
+    document.getElementById('number').innerHTML = "This firstnumber is " + geometry (firstNumber, secondNumber);
+    console.log("The secondnumber is", geometry (firstNumber, secondNumber) );
+    document.getElementById('number1').innerHTML = "This secondnumber is " + geometry (firstNumber, secondNumber);
+  }
+
+  }
